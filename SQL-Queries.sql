94614512fad95885a7735a7ec22f0f804b2ae49d
aipie <aiitrann@gmail.com>
2025-03-31 19:56:24 +0900

    Update SQL-Queries.sql

--- SQL-Queries.sql
+++ SQL-Queries.sql
@@ -10,3 +10,24 @@ SET ride_length_minutes =
   SAFE_CAST(SPLIT(CAST(ride_length AS STRING), ":")[SAFE_OFFSET(1)] AS INT) + 
   -- Extract seconds from `ride_length`, convert to float, and divide by 60 to get fractional minutes
   SAFE_CAST(SPLIT(CAST(ride_length AS STRING), ":")[SAFE_OFFSET(2)] AS FLOAT64) / 60;
+
+-- Adding distance column 
+ALTER TABLE `nimble-volt-436804-j6.bike_share.combined_data`
+ADD COLUMN distance FLOAT64;
+
+-- Using the Haversine formula to calculate the distance for each ride 
+UPDATE `nimble-volt-436804-j6.bike_share.combined_data`
+SET distance_km = 2 * 6371 * ASIN(SQRT(
+      POWER(SIN((end_lat - start_lat) * 3.141592653589793 / 180 / 2), 2) +
+      COS(start_lat * 3.141592653589793 / 180) * COS(end_lat * 3.141592653589793 / 180) *
+      POWER(SIN((end_lng - start_lng) * 3.141592653589793 / 180 / 2), 2)
+));
+
+-- Calculating rides for members and casual riders 
+SELECT 
+  member_casual, count(*) AS ride_count 
+FROM
+  `nimble-volt-436804-j6.bike_share.combined_final_table`
+GROUP BY 
+  member_casual 
+  
